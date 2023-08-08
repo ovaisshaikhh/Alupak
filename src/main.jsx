27452225dd03cd397f8112a251944f9f163db571
[Wajiha Arain] <wajiha.hanif@siardigital.com>
2023-08-08 11:24:00 +0500

    aws

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
+import { Amplify } from "aws-amplify";
+// import Amplify, { Auth } from "aws-amplify";
+
+Amplify.configure({});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
